7abeaabd55c887f1a2fe1843cb459056ade31922
Matheus Leite <matheus.leite.san@gmail.com>
2022-11-21 21:54:35 -0300

    Improve SimpleRouter

--- php-arch/src/route/SimpleRouter.php
+++ php-arch/src/route/SimpleRouter.php
@@ -9,35 +9,45 @@ use ReflectionMethod;
 
 class SimpleRouter implements RouterInterface
 {
-    private const NOT_FOUND_CODE = 204;
+    private const NOT_FOUND_CODE = 404;
+    private const INTERNAL_ERROR_CODE = 500;
     private array $routes = [];
 
+    /**
+     * @param string $requestAction
+     * @param array $params
+     * @return JsonResponse
+     */
     public function dispatch(string $requestAction, array $params = []): JsonResponse
     {
         if (!key_exists($requestAction, $this->routes)) {
-            throw new InvalidArgumentException("'$requestAction' is an unregistered route", self::NOT_FOUND_CODE);
+            return JsonResponse::getInstance()
+                ->setData(['status' => self::NOT_FOUND_CODE, 'message' => "Invalid resource"])
+                ->setStatusCode(self::NOT_FOUND_CODE);
         }
 
-        $uriContent = $this->routes[$requestAction];
-        $reflectedControllerMethod = self::createReflectionMethod($uriContent);
-
-        return $reflectedControllerMethod->invokeArgs(new $uriContent['namespace'], $params);
+        try {
+            $uriContent = $this->routes[$requestAction];
+            $reflectedControllerMethod = new ReflectionMethod($uriContent['namespace'], $uriContent['method']);
+            return $reflectedControllerMethod->invokeArgs(new $uriContent['namespace'], $params);
+        } catch (ReflectionException $exception) {
+            return JsonResponse::getInstance()
+                ->setData(['status' => self::INTERNAL_ERROR_CODE, 'message' => "Internal server error"])
+                ->setStatusCode(self::INTERNAL_ERROR_CODE);
+        }
     }
 
+    /**
+     * @param string $uri
+     * @param string $controller
+     * @param string $method
+     * @return void
+     */
     public function registry(string $uri, string $controller, string $method): void
     {
         $this->routes[$uri] = [
             'method' => $method,
             'namespace' => $controller,
         ];
-    }
-
-    private static function createReflectionMethod(array $uriContent): ReflectionMethod
-    {
-        try {
-            return new ReflectionMethod($uriContent['namespace'], $uriContent['method']);
-        } catch (ReflectionException $exception) {
-            print $exception->getMessage();
-        }
     }
 }
